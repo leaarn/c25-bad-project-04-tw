6b130e62861de963195039b7b526717b8b93241f
Julia <beyourdetective@gmail>
2023-03-27 19:13:29 +0800

    completed init database

--- init_db.sql
+++ init_db.sql
@@ -1,86 +1,90 @@
+DROP DATABASE IF EXISTS project2;
+CREATE DATABASE project2;
+
 \c project2
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	last_name VARCHAR NOT NULL,
-	first_name VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
-	password VARCHAR NOT NULL,
-	contact_num INT NOT NULL,
-	default_district VARCHAR NOT NULL,
-	default_address VARCHAR NOT NULL,
-	default_coordinates VARCHAR NOT NULL,
-	created_at TIMESTAMP DEFAULT NOW(),
-	updated_at TIMESTAMP DEFAULT NOW()
+	"id" SERIAL PRIMARY KEY,
+	"last_name" VARCHAR(255) NOT NULL,
+    "first_name" VARCHAR(255) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "contact_num" INTEGER NOT NULL,
+    "default_district" VARCHAR(255) NOT NULL,
+    "default_address" VARCHAR(255) NOT NULL,
+    "default_coordinates" POINT NOT NULL,
+    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    "updated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
 );
 
 DROP TABLE IF EXISTS drivers;
 CREATE TABLE drivers (
-	id SERIAL PRIMARY KEY,
-	last_name VARCHAR NOT NULL,
-	first_name VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
-	password VARCHAR NOT NULL,
-	contact_num INT NOT NULL,
-	car_license_num VARCHAR NOT NULL,
-	car_type VARCHAR NOT NULL,
-	created_at TIMESTAMP DEFAULT NOW(),
-	updated_at TIMESTAMP DEFAULT NOW()
+	"id" SERIAL PRIMARY KEY,
+	"last_name" VARCHAR(255) NOT NULL,
+    "first_name" VARCHAR(255) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "contact_num" INTEGER NOT NULL,
+    "car_license_num" VARCHAR(255) NOT NULL,
+    "car_type" VARCHAR(255) NOT NULL,
+	"created_at" TIMESTAMP DEFAULT NOW(),
+	"updated_at" TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
-	id SERIAL PRIMARY KEY,
-	pick_up_date DATE,
-	pick_up_time TIME,
-	pick_up_district VARCHAR NOT NULL,
-	pick_up_address VARCHAR NOT NULL,
-	pick_up_coordinates POINT,
-	deliver_district VARCHAR NOT NULL,
-	deliver_address VARCHAR NOT NULL,
-	deliver_coordinates POINT,
+	"id" SERIAL PRIMARY KEY,
+	"pick_up_date" DATE,
+	"pick_up_time" TIME(0) WITHOUT TIME ZONE NOT NULL,
+    "pick_up_district" VARCHAR(255) NOT NULL,
+    "pick_up_address" VARCHAR(255) NOT NULL,
+    "pick_up_coordinates" POINT NOT NULL,
+    "deliver_district" VARCHAR(255) NOT NULL,
+    "deliver_address" VARCHAR(255) NOT NULL,
+    "deliver_coordinates" POINT NOT NULL,
 	FOREIGN KEY (users_id) REFERENCES users(id),
 	FOREIGN KEY (drivers_id) REFERENCES drivers(id),
-	distance_km INT NOT NULL,
-	distance_price INT NOT NULL,
-	reference_code uuid DEFAULT uuid_generate_v4(),
-	order_status VARCHAR NOT NULL,
-	token VARCHAR,
-	remarks VARCHAR,
-	created_at TIMESTAMP DEFAULT NOW(),
-	updated_at TIMESTAMP DEFAULT NOW()
+	"distance_km" INTEGER NOT NULL,
+    "distance_price" INTEGER NOT NULL,
+    "reference_code" UUID NOT NULL,
+    "order_status" VARCHAR(255) NULL,
+    "token" VARCHAR(255) NULL,
+    "remarks" VARCHAR(255) NULL,
+    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+	"updated_at" TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS car_types;
 CREATE TABLE car_types (
-	id SERIAL PRIMARY KEY,
-	car_type VARCHAR NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"car_type" VARCHAR(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS payment_method;
 CREATE TABLE payment_method (
-	id SERIAL PRIMARY KEY,
-	method VARCHAR NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"method" VARCHAR NOT NULL
 );
 
 DROP TABLE IF EXISTS order_animals;
 CREATE TABLE order_animals(
-	id SERIAL PRIMARY KEY,
+	"id" SERIAL PRIMARY KEY,
 	FOREIGN KEY (orders_id) REFERENCES orders(id),
 	FOREIGN KEY (animals_id) REFERENCES animals(id),
-	animals_amount INT NOT NULL,
-	animals_unit_price INT NOT NULL
+    "animals_amount" INTEGER NOT NULL,
+    "animals_unit_price" INTEGER NOT NULL
 
 );
 
 DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
-	id SERIAL PRIMARY KEY,
-	animals_name VARCHAR NOT NULL,
-	price INT
+	"id" SERIAL PRIMARY KEY,
+	"animals_name" VARCHAR(255) NOT NULL,
+    "price" INTEGER NOT NULL
+
 );
 
 
--- insert_data.ts
+++ insert_data.ts
@@ -1,33 +1,112 @@
 import path from "path";
 import xlsx from "xlsx";
+import { hashPassword } from "./utils/hash";
+
 import { Client } from "pg";
 
 import dotenv from "dotenv";
 dotenv.config();
-const client = new pg.Client({
-  database: process.env.DB_NAME,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-});
+
+export interface UsersRow {
+  last_name: string;
+  first_name: string;
+  title: string;
+  email: string;
+  password: string;
+  contact_num: Number;
+  default_district: string;
+  default_address: string;
+  default_coordinates?: string;
+  created_at?: string;
+  updated_at?: string;
+}
+
+export interface DriversRow {
+  last_name: string;
+  first_name: string;
+  title: string;
+  email: string;
+  password: string;
+  contact_num: Number;
+  car_license_num: Number;
+  car_type: Number;
+  created_at?: string;
+  updated_at?: string;
+}
+
+export interface OrdersRow {
+  pick_up_date: Date;
+  pick_up_time: TimeRanges;
+  pick_up_district: string;
+  pick_up_address: string;
+  pick_up_coordinates?: string;
+  deliver_district: string;
+  deliver_address: string;
+  deliver_coordinates?: string;
+  distance_km: Number;
+  distance_price: Number;
+  reference_code: string;
+  order_status: string;
+  token: string;
+  remarks: string;
+  created_at?: string;
+  updated_at?: string;
+}
+
+export interface CarTypesRow {
+  car_type: string;
+}
+
+export interface PaymentMethodRow {
+  method: string;
+}
+
+export interface OrderAnimalsRow {
+  animals_amount: Number;
+  animals_unit_price: Number;
+}
+
+export interface AnimalsRow {
+  animals_name: string;
+  price: Number;
+}
 
 async function main() {
-  await client.connect(); // "dial-in" to the postgres server
-  const user = {
-    username: "gordon",
-    password: "tecky",
-  };
+  const client = new Client({
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  });
 
-  await client.query("INSERT INTO users (username,password) values ($1,$2)", [
-    user.username,
-    user.password,
-  ]);
+  const filepath = path.join(__dirname, "");
+  const workbook = xlsx.readFile(filepath);
 
-  const result = await client.query("SELECT * from users where username = $1", [
-    "gordon",
-  ]);
+  const userRows = xlsx.utils.sheet_to_json<UsersRow>(workbook.Sheets["user"]);
+  const driversRow = xlsx.utils.sheet_to_json<DriversRow>(
+    workbook.Sheets["memo"]
+  );
+  const ordersRow = xlsx.utils.sheet_to_json<OrdersRow>(
+    workbook.Sheets["user"]
+  );
+  const carTypesRow = xlsx.utils.sheet_to_json<CarTypesRow>(
+    workbook.Sheets["memo"]
+  );
+  const paymentMethodRow = xlsx.utils.sheet_to_json<PaymentMethodRow>(
+    workbook.Sheets["memo"]
+  );
+  const orderAnimalsRow = xlsx.utils.sheet_to_json<OrderAnimalsRow>(
+    workbook.Sheets["user"]
+  );
+  const animalsRow = xlsx.utils.sheet_to_json<AnimalsRow>(
+    workbook.Sheets["memo"]
+  );
 
-  console.log(result.rows[0].username); // gordon
-  await client.end(); // close connection with the database
+  await client.connect();
+
+  
+
+  await client.end();
 }
 
 main();
+console.log("hi");
