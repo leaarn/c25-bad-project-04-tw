b44a43c601b3300080d2c454c9858a97c68a8d64
Julia <beyourdetective@gmail>
2023-03-27 20:04:16 +0800

    insert data

--- init_db.sql
+++ init_db.sql
@@ -5,85 +5,85 @@ CREATE DATABASE project2;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	"id" SERIAL PRIMARY KEY,
-	"last_name" VARCHAR(255) NOT NULL,
-    "first_name" VARCHAR(255) NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
-    "contact_num" INTEGER NOT NULL,
-    "default_district" VARCHAR(255) NOT NULL,
-    "default_address" VARCHAR(255) NOT NULL,
-    "default_coordinates" POINT NOT NULL,
-    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    "updated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
+	id SERIAL PRIMARY KEY,
+	last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    contact_num INTEGER NOT NULL,
+    default_district VARCHAR(255) NOT NULL,
+    default_address VARCHAR(255) NOT NULL,
+    default_coordinates POINT NOT NULL,
+    created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
 );
 
 DROP TABLE IF EXISTS drivers;
 CREATE TABLE drivers (
-	"id" SERIAL PRIMARY KEY,
-	"last_name" VARCHAR(255) NOT NULL,
-    "first_name" VARCHAR(255) NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
-    "contact_num" INTEGER NOT NULL,
-    "car_license_num" VARCHAR(255) NOT NULL,
-    "car_type" VARCHAR(255) NOT NULL,
-	"created_at" TIMESTAMP DEFAULT NOW(),
-	"updated_at" TIMESTAMP DEFAULT NOW()
+	id SERIAL PRIMARY KEY,
+	last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    contact_num INTEGER NOT NULL,
+    car_license_num VARCHAR(255) NOT NULL,
+    car_type VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	updated_at TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
-	"id" SERIAL PRIMARY KEY,
-	"pick_up_date" DATE,
-	"pick_up_time" TIME(0) WITHOUT TIME ZONE NOT NULL,
-    "pick_up_district" VARCHAR(255) NOT NULL,
-    "pick_up_address" VARCHAR(255) NOT NULL,
-    "pick_up_coordinates" POINT NOT NULL,
-    "deliver_district" VARCHAR(255) NOT NULL,
-    "deliver_address" VARCHAR(255) NOT NULL,
-    "deliver_coordinates" POINT NOT NULL,
+	id SERIAL PRIMARY KEY,
+	pick_up_date DATE,
+	pick_up_time TIME(0) WITHOUT TIME ZONE NOT NULL,
+    pick_up_district VARCHAR(255) NOT NULL,
+    pick_up_address VARCHAR(255) NOT NULL,
+    pick_up_coordinates POINT NOT NULL,
+    deliver_district VARCHAR(255) NOT NULL,
+    deliver_address VARCHAR(255) NOT NULL,
+    deliver_coordinates POINT NOT NULL,
 	FOREIGN KEY (users_id) REFERENCES users(id),
 	FOREIGN KEY (drivers_id) REFERENCES drivers(id),
-	"distance_km" INTEGER NOT NULL,
-    "distance_price" INTEGER NOT NULL,
-    "reference_code" UUID NOT NULL,
-    "order_status" VARCHAR(255) NULL,
-    "token" VARCHAR(255) NULL,
-    "remarks" VARCHAR(255) NULL,
-    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-	"updated_at" TIMESTAMP DEFAULT NOW()
+	distance_km INTEGER NOT NULL,
+    distance_price INTEGER NOT NULL,
+    reference_code UUID NOT NULL,
+    order_status VARCHAR(255) NULL,
+    token VARCHAR(255) NULL,
+    remarks VARCHAR(255) NULL,
+    created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+	updated_at TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS car_types;
 CREATE TABLE car_types (
-	"id" SERIAL PRIMARY KEY,
-	"car_type" VARCHAR(255) NOT NULL
+	id SERIAL PRIMARY KEY,
+	car_type VARCHAR(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS payment_method;
 CREATE TABLE payment_method (
-	"id" SERIAL PRIMARY KEY,
-	"method" VARCHAR NOT NULL
+	id SERIAL PRIMARY KEY,
+	method VARCHAR NOT NULL
 );
 
 DROP TABLE IF EXISTS order_animals;
 CREATE TABLE order_animals(
-	"id" SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	FOREIGN KEY (orders_id) REFERENCES orders(id),
 	FOREIGN KEY (animals_id) REFERENCES animals(id),
-    "animals_amount" INTEGER NOT NULL,
-    "animals_unit_price" INTEGER NOT NULL
+    animals_amount INTEGER NOT NULL,
+    animals_unit_price INTEGER NOT NULL
 
 );
 
 DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
-	"id" SERIAL PRIMARY KEY,
-	"animals_name" VARCHAR(255) NOT NULL,
-    "price" INTEGER NOT NULL
+	id SERIAL PRIMARY KEY,
+	animals_name VARCHAR(255) NOT NULL,
+    price INTEGER NOT NULL
 
 );
 
--- insert_data.ts
+++ insert_data.ts
@@ -81,36 +81,46 @@ async function main() {
   const filepath = path.join(__dirname, "");
   const workbook = xlsx.readFile(filepath);
 
-  const userRows = xlsx.utils.sheet_to_json<UsersRow>(workbook.Sheets["user"]);
+  const userRows = xlsx.utils.sheet_to_json<UsersRow>(workbook.Sheets["users"]);
   const driversRow = xlsx.utils.sheet_to_json<DriversRow>(
-    workbook.Sheets["memo"]
+    workbook.Sheets["drivers"]
   );
   const ordersRow = xlsx.utils.sheet_to_json<OrdersRow>(
-    workbook.Sheets["user"]
+    workbook.Sheets["orders"]
   );
   const carTypesRow = xlsx.utils.sheet_to_json<CarTypesRow>(
-    workbook.Sheets["memo"]
+    workbook.Sheets["car_types"]
   );
   const paymentMethodRow = xlsx.utils.sheet_to_json<PaymentMethodRow>(
-    workbook.Sheets["memo"]
+    workbook.Sheets["payment_method"]
   );
   const orderAnimalsRow = xlsx.utils.sheet_to_json<OrderAnimalsRow>(
-    workbook.Sheets["user"]
+    workbook.Sheets["order_animals"]
   );
   const animalsRow = xlsx.utils.sheet_to_json<AnimalsRow>(
-    workbook.Sheets["memo"]
+    workbook.Sheets["animals"]
   );
 
   await client.connect();
   await client.query(/*SQL*/ `DELETE FROM users`);
   for (const userRow of userRows) {
-    const userSql = /*SQL*/ `INSERT INTO users (email, password) VALUES ($1, $2, $3)`;
+    const userSql = /*SQL*/ `INSERT INTO users (last_name, first_name, title, email, password, contact_num, default_district, default_address, default_coordinates) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
     let hashed = await hashPassword(userRow.password);
-    await client.query(userSql, [userRow.email, hashed]);
+    await client.query(userSql, [
+      userRow.last_name,
+      userRow.first_name,
+      userRow.title,
+      userRow.email,
+      hashed,
+      userRow.contact_num,
+      userRow.default_district,
+      userRow.default_address,
+      userRow.default_coordinates,
+    ]);
   }
 
   await client.query(/*SQL*/ `DELETE FROM drivers`);
-  let driversSql = `INSERT INTO drivers (content) VALUES `;
+  let driversSql = `INSERT INTO drivers (last_name, first_name, title, email, password, contact_num, car_license_num, car_type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) `;
   for (let i = 0; i < driversRow.length; i++) {
     if (i < driversRow.length - 1) driversSql += `($${i + 1}),`;
     else driversSql += `($${i + 1})`;
@@ -118,9 +128,18 @@ async function main() {
   console.log(driversSql);
   await client.query(
     driversSql,
-    driversRow.map((row) => row.content)
+    driversRow.map((row) => [
+      row.last_name,
+      row.last_name,
+      row.first_name,
+      row.title,
+      row.email,
+      row.password,
+      row.contact_num,
+      row.car_license_num,
+      row.car_type,
+    ])
   );
-
 
   await client.end();
 }
