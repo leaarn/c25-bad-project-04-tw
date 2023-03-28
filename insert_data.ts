7516c6c41ee6daf87e55ed90d29289ab5eaf8e71
Julia <beyourdetective@gmail.com>
2023-03-28 09:55:27 +0800

    "test"

--- insert_data.ts
+++ insert_data.ts
@@ -1,75 +1,20 @@
 import path from "path";
 import xlsx from "xlsx";
 import { hashPassword } from "./utils/hash";
-
 import { Client } from "pg";
+import { UsersRow } from "./model";
+import { DriversRow } from "./model";
+import { OrdersRow } from "./model";
+import { CarTypesRow } from "./model";
+import { PaymentMethodRow } from "./model";
+import { OrderAnimalsRow } from "./model";
+import { AnimalsRow } from "./model";
+
 
 import dotenv from "dotenv";
 dotenv.config();
 
-export interface UsersRow {
-  last_name: string;
-  first_name: string;
-  title: string;
-  email: string;
-  password: string;
-  contact_num: Number;
-  default_district: string;
-  default_address: string;
-  default_coordinates?: string;
-  created_at?: string;
-  updated_at?: string;
-}
 
-export interface DriversRow {
-  last_name: string;
-  first_name: string;
-  title: string;
-  email: string;
-  password: string;
-  contact_num: Number;
-  car_license_num: Number;
-  car_type: Number;
-  created_at?: string;
-  updated_at?: string;
-}
-
-export interface OrdersRow {
-  pick_up_date: Date;
-  pick_up_time: TimeRanges;
-  pick_up_district: string;
-  pick_up_address: string;
-  pick_up_coordinates?: string;
-  deliver_district: string;
-  deliver_address: string;
-  deliver_coordinates?: string;
-  distance_km: Number;
-  distance_price: Number;
-  reference_code: string;
-  order_status: string;
-  token: string;
-  remarks: string;
-  created_at?: string;
-  updated_at?: string;
-}
-
-export interface CarTypesRow {
-  car_type: string;
-}
-
-export interface PaymentMethodRow {
-  method: string;
-}
-
-export interface OrderAnimalsRow {
-  animals_amount: Number;
-  animals_unit_price: Number;
-}
-
-export interface AnimalsRow {
-  animals_name: string;
-  price: Number;
-}
 
 async function main() {
   const client = new Client({
---
+++ login.ts
@@ -0,0 +1,70 @@
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
+import path from "path";
+import fs from "fs";
+import expressSession from "express-session";
+
+
+const app = express();
+
+// Section 1: Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  expressSession({
+    secret: "Tecky Academy teaches typescript",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+declare module "express-session" {
+  interface SessionData {
+    isLoggedIn?: boolean;
+  }
+}
+
+// logging 用 middleware
+app.use((req, _res, next) => {
+  console.log(`Path ${req.path}, Method: ${req.method}`);
+  next();
+});
+
+app.post("/login", async (req, res) => {
+  const username: string = req.body.username;
+  const password: string = req.body.password;
+  if (!username || !password) {
+    res.status(400).json({ message: "missing username or password" });
+    return;
+  }
+
+  const users: Array<Users> = await jsonfile.readFile(USER_JSON_PATH);
+  const foundUser = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (!foundUser) {
+    res.status(400).json({ message: "invalid username or password" });
+    return;
+  }
+
+  req.session.isLoggedIn = true;
+  // res.json({message: "login success"})
+  res.redirect("/admin.html");
+});
+
+// Section 3: Serve
+app.use(express.static(path.join(__dirname, "public")));
+const guardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.session.isLoggedIn) next();
+  else res.redirect("/");
+};
+app.use(guardMiddleware, express.static(path.join(__dirname, "private")));
+
+// Section 4: Error Handling
+app.use((_req, res) => {
+  res.sendFile(path.join(__dirname, "public", "404.html"));
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`);
+});
