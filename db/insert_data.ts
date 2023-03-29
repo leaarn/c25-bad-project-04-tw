216c3af4230fcc0e6bef8462c2297401e867bef7
Chinny <learning20150133@gmail.com>
2023-03-29 17:13:28 +0800

    get createOrder id

--- db/insert_data.ts
+++ db/insert_data.ts
@@ -13,12 +13,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-    const client = new Client({
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-    });
-    
+  const client = new Client({
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  });
+
   const filepath = path.join(__dirname, "data_base.xlsx");
   const workbook = xlsx.readFile(filepath);
 
@@ -67,28 +67,28 @@ async function main() {
     ]);
   }
 
-    await client.query(/*SQL*/ `DELETE FROM orders`);
-    for (const orderRow of ordersRow) {
-      let ordersSql = /*SQL*/ `INSERT INTO orders (pick_up_date, pick_up_time, pick_up_district, pick_up_address, pick_up_coordinates, deliver_district, deliver_address, deliver_coordinates, receiver_name, receiver_contact, distance_km, distance_price, reference_code, orders_status, token, remarks) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)`;
-      await client.query(ordersSql, [
-        orderRow.pick_up_date,
-        orderRow.pick_up_time,
-        orderRow.pick_up_district,
-        orderRow.pick_up_address,
-        orderRow.pick_up_coordinates,
-        orderRow.receiver_name,
-        orderRow.receiver_contact,
-        orderRow.deliver_district,
-        orderRow.deliver_address,
-        orderRow.deliver_coordinates,
-        orderRow.distance_km,
-        orderRow.distance_price,
-        orderRow.reference_code,
-        orderRow.order_status,
-        orderRow.token,
-        orderRow.remarks,
-      ]);
-    };
+  await client.query(/*SQL*/ `DELETE FROM orders`);
+  for (const orderRow of ordersRow) {
+    let ordersSql = /*SQL*/ `INSERT INTO orders (pick_up_date, pick_up_time, pick_up_district, pick_up_address, pick_up_coordinates, receiver_name, receiver_contact, deliver_district, deliver_address, deliver_coordinates, distance_km, distance_price, reference_code, orders_status, token, remarks) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)`;
+    await client.query(ordersSql, [
+      orderRow.pick_up_date,
+      orderRow.pick_up_time,
+      orderRow.pick_up_district,
+      orderRow.pick_up_address,
+      orderRow.pick_up_coordinates,
+      orderRow.receiver_name,
+      orderRow.receiver_contact,
+      orderRow.deliver_district,
+      orderRow.deliver_address,
+      orderRow.deliver_coordinates,
+      orderRow.distance_km,
+      orderRow.distance_price,
+      orderRow.reference_code,
+      orderRow.order_status,
+      orderRow.token,
+      orderRow.remarks,
+    ]);
+  }
 
   await client.query(/*SQL*/ `DELETE FROM payment_method`);
   for (const paymentRow of paymentMethodRow) {
