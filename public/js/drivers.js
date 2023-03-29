4cfa304007e47f40056ebadb7dd6f7e9ce959766
dora dora <dora_1324@gmail.com>
2023-03-29 11:45:32 +0800

    update order db

--- public/js/drivers.js
+++ public/js/drivers.js
@@ -5,14 +5,28 @@ window.onload = async () => {
     return;
   }
 
-  const resp = await fetch(`/pokemon/${urlSearchParams.get("pid")}`);
-  const pokemon = await resp.json();
-  console.log(pokemon);
+  const resp = await fetch(`/drivers/${urlSearchParams.get("oid")}`);
+  const orders = await resp.json();
+  console.log(orders);
 
-  const loginBtn = `<button onClick="login()">Login</button>`;
-  const addPokemon = `<button onClick="addPokemon(${pokemon.id})">Add Pokemon</button>`;
+  const acceptOrder = `<button onClick="acceptOrder(${orders.id})">接單</button>`;
 
   let htmlStr = `
+      <div class="available_orders">
+        <div class="order_details">
+            <p class="from">FROM</p>
+            <div class="deli_district_datetime">
+                <div class="deli_district">${orders.deliver_district}</div>
+                <div class="deli_datetime"></div>
+            </div>
+            <p class="to">TO</p>
+            <div class="pick_up_district_animals">
+                <div class="pick_up_district"></div>
+                <div class="animals"></div>
+            </div>
+        </div>
+        ${acceptOrder}
+      </div>
       <h1>${pokemon.name}</h1>
       <p>Type 1: ${pokemon.type1}</p>
       <p>Type 2: ${pokemon.type2}</p>
