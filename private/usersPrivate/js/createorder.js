2aae8770c449fab7909739e6c6b9c373faae4e2c
Chinny <learning20150133@gmail.com>
2023-03-31 12:35:06 +0800

    createoder.ts for loop animals_id

--- private/usersPrivate/js/createorder.js
+++ private/usersPrivate/js/createorder.js
@@ -20,6 +20,7 @@ function createOrder() {
     const pick_up_date = form.date.value;
     const pick_up_time = form.time.value;
     const pick_up_district = form.pickUpDistrict.value;
+    console.log("pick up", typeof pick_up_district);
     //   const pick_up_address =
     //     form.pickUpRoom.value +
     //     ", " +
@@ -28,12 +29,12 @@ function createOrder() {
     //     form.pickUpBuilding.value +
     //     ", " +
     //     form.pickUpStreet.value;
-    const pick_up_room = form.pickUpRoom;
-    const pick_up_floor = form.pickUpFloor;
-    const pick_up_building = form.pickUpBuilding;
-    const pick_up_street = form.pickUpStreet;
+    const pick_up_room = form.pickUpRoom.value;
+    const pick_up_floor = form.pickUpFloor.value;
+    const pick_up_building = form.pickUpBuilding.value;
+    const pick_up_street = form.pickUpStreet.value;
     // const pick_up_coordinates =form.pick_up_coordinates
-    const deliver_district = form.deliver_district.value;
+    const deliver_district = form.deliverDistrict.value;
     //   const deliver_address =
     //     form.deliverRoom.value +
     //     ", " +
@@ -42,10 +43,10 @@ function createOrder() {
     //     form.deliverBuilding.value +
     //     ", " +
     //     form.deliverStreet.value;
-    const deliver_room = form.deliverRoom;
-    const deliver_floor = form.deliverFloor;
-    const deliver_building = form.deliverBuilding;
-    const deliver_street = form.deliverStreet;
+    const deliver_room = form.deliverRoom.value;
+    const deliver_floor = form.deliverFloor.value;
+    const deliver_building = form.deliverBuilding.value;
+    const deliver_street = form.deliverStreet.value;
     // const deliver_coordinates =form.deliver_coordinates
     const receiver_name = form.receiverName.value;
     const receiver_contact = form.receiver_contact.value;
@@ -54,25 +55,25 @@ function createOrder() {
     for (const select of animals_id_selects) {
       animals_id.push(select.value);
     }
-    console.log(animals_id);
+    console.log("here is id", animals_id);
     const animals_amount = [];
     const animals_amount_selects = form.querySelectorAll("select[name=animals_amount]");
     let total = 0;
     for (const select of animals_amount_selects) {
-      animals_amount_selects.push(select.value);
+      animals_amount.push(select.value);
       total += parseInt(select.value);
     }
     if (total > 5) {
       alert("Too many animals la...");
       return;
     }
-    console.log(animals_amount);
+    console.log("here is amount", animals_amount);
 
     const remarks = form.remarks.value;
 
     const resp = await fetch("/usersMain", {
       method: "POST",
-      header: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         pick_up_date,
         pick_up_time,
--- routers/createorder.ts
+++ routers/createorder.ts
@@ -1,36 +1,60 @@
 import { dbClient } from "./../app";
 import type { Request, Response } from "express";
 import express from "express";
-
 import { userIsLoggedInApi } from "../utils/guard";
+import { logger } from "../utils/logger";
 
 export const createOrderRoutes = express.Router();
 createOrderRoutes.post("/", userIsLoggedInApi, createOrder);
 
 async function createOrder(req: Request, res: Response) {
-  const pick_up_date = req.body.date;
-  const pick_up_time = req.body.time;
-  const pick_up_district = req.body.pickUpDistrict;
-  const pick_up_room = req.body.pickUpRoom;
-  const pick_up_floor = req.body.pickUpFloor;
-  const pick_up_building = req.body.pickUpBuilding;
-  const pick_up_street = req.body.pickUpStreet;
+  // try {
+  const pick_up_date = req.body.pick_up_date;
+  const pick_up_time = req.body.pick_up_time;
+  const pick_up_district = req.body.pick_up_district;
+  console.log(pick_up_district);
+  const pick_up_room = req.body.pick_up_room;
+  const pick_up_floor = req.body.pick_up_floor;
+  const pick_up_building = req.body.pick_up_building;
+  const pick_up_street = req.body.pick_up_street;
 
   // const pick_up_coordinates =req.body.pick_up_coordinates
   const deliver_district = req.body.deliver_district;
-  const deliver_room = req.body.deliverRoom;
-  const deliver_floor = req.body.deliverFloor;
-  const deliver_building = req.body.deliverBuilding;
-  const deliver_street = req.body.deliverStreet;
+  const deliver_room = req.body.deliver_room;
+  const deliver_floor = req.body.deliver_floor;
+  const deliver_building = req.body.deliver_building;
+  const deliver_street = req.body.deliver_street;
 
   // const deliver_coordinates =req.body.deliver_coordinates
+  //??
   const users_id = req.session.users_id!;
-  const receiver_name = req.body.receiverName;
+  const receiver_name = req.body.receiver_name;
   const receiver_contact = req.body.receiver_contact;
   const animals_id = req.body.animals_id;
   const animals_amount = req.body.animals_amount;
   const remarks = req.body.remarks;
   const distance_km = Math.round(Math.random() * (100 - 1) + 1);
+
+  logger.info(`
+here is logger
+${pick_up_date},
+${pick_up_time},
+${pick_up_district},
+${pick_up_room},
+${pick_up_floor},
+${pick_up_building},
+${deliver_street},
+${deliver_district},
+${deliver_room},
+${deliver_floor},
+${deliver_building},
+${deliver_street},
+${users_id},
+${receiver_name},
+${receiver_contact},
+${animals_id},
+${animals_amount},
+${remarks}`);
   res.status(200).json({ message: "success" });
 
   const createOrderId = (
@@ -61,18 +85,24 @@ async function createOrder(req: Request, res: Response) {
   console.log("here is testing", createOrderId);
 
   //未有加ANIMAL 和 最多5隻
-  const animalPrice = await dbClient.query(/*SQL*/ `SELECT price from animals where id = $1`, [animals_id]);
-  const animals_history_price = animalPrice.rows[0].price;
-  // console.log("here is anm price", animals_history_price);
+  for (let i = 0; i < animals_id.length; i++) {
+    const animalPrice = await dbClient.query(/*SQL*/ `SELECT price from animals where id = $1`, [parseInt(animals_id)]);
+    const animals_history_price = animalPrice.rows[0].price;
+    // console.log("here is anm price", animals_history_price);
 
-  //insert table order_animals
-  const orderAnimal = await dbClient.query(
-    /*SQL*/ `INSERT INTO order_animals (orders_id,animals_id,animals_amount,animals_history_price)
+    //insert table order_animals
+    const orderAnimal = await dbClient.query(
+      /*SQL*/ `INSERT INTO order_animals (orders_id,animals_id,animals_amount,animals_history_price)
     VALUES ($1,$2,$3,$4)`,
-    [createOrderId, animals_id, animals_amount, animals_history_price]
-  );
-  console.log("here is order anm", orderAnimal);
+      [createOrderId, parseInt(animals_id), parseInt(animals_amount[i]), animals_history_price]
+    );
+    console.log("here is order anm", orderAnimal);
+  }
 }
+// } catch (e) {
+//   console.error(e);
+//   res.status(500).json({ error: "failed to create order" });
+// }
 
 // const data = {
 //   	"date":"2020-01-01",
@@ -95,18 +125,3 @@ async function createOrder(req: Request, res: Response) {
 // 	"animals_amount":"1",
 //  "remarks":"hihihi"
 // }
-
-// logger.info(`
-// here is logger
-// ${pick_up_date},
-// ${pick_up_time},
-// ${pick_up_district},
-// ${pick_up_address},
-// ${deliver_district},
-// ${deliver_address},
-// ${users_id},
-// ${receiver_name},
-// ${receiver_contact},
-// ${animals_id},
-// ${animals_amount},
-// ${remarks}`);
