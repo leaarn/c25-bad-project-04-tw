04cefd3308f24da159ce851d970365c820bfc91b
Julia <beyourdetective@gmail.com>
2023-04-03 13:21:50 +0800

    data

---
+++ public/js/receivers.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+  receiversForm();
+};
+
+function receiversForm() {
+  const form = document.querySelector("#receivers-form");
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const token = form.token.value;
+    const resp = await fetch("/receivertoken/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token }),
+    });
+
+    if (resp.status === 200) {
+      alert("Thank you for choosing us!")
+    } else {
+      const data = await resp.json();
+      alert(data.message);
+    }
+  });
+}
+
