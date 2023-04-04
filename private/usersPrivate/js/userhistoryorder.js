6a637c6f018468b57f064f04f39f22a713e36e46
Chinny <learning20150133@gmail.com>
2023-04-04 20:36:55 +0800

    historder js

--- private/usersPrivate/js/userhistoryorder.js
+++ private/usersPrivate/js/userhistoryorder.js
@@ -13,18 +13,25 @@ async function loadCompleteOrder() {
   const title = document.createElement("h3");
   orderContainer.appendChild(title);
   //iterate orders complete
-  for (const order of complete){
+  for (const order of complete) {
     //create element
-    const referenceCode = document.createElement("p")
-    const orderStatus = document.createElement("p")
-    const animals = document.createElement("p")
+    const referenceCode = document.createElement("p");
+    const orderStatus = document.createElement("p");
+    const animals = document.createElement("p");
     //button for more details
-    const detailsBtn = document.createElement("button")
+    const detailsBtn = document.createElement("button");
     //text content
-    referenceCode.textContent = `訂單號碼 : ${order.order_status}`
-    orderStatus.textContent = `訂單狀態 : ${}`
-    animals.textContent = `${}`
-    detailsBtn.textContent = ``
-
+    referenceCode.textContent = `訂單號碼 : ${order.reference_code}`;
+    orderStatus.textContent = `訂單狀態 : ${order.order_status}`;
+    let animalDetails = ``;
+    if (Array.isArray(order.animals_name)) {
+      for (let i = 0; i < order.animals_name.length; i++) {
+        animalDetails += order.animals_name[i] + " X " + order.animals_amount[i] + " ";
+      }
+    } else {
+      animalDetails += order.animals_name + " X " + order.animals_amount + " ";
+    }
+    animals.textContent = `動物 : ${animalDetails}`;
+    detailsBtn.textContent = `查看更多`;
   }
 }
