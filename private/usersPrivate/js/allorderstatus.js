94b062a82bcc9ac6db03f92645b3fb9cdff03a0c
Chinny <learning20150133@gmail.com>
2023-04-06 17:38:44 +0800

    allorderstatus css

--- private/usersPrivate/js/allorderstatus.js
+++ private/usersPrivate/js/allorderstatus.js
@@ -26,10 +26,66 @@ async function allOrderStatus() {
     // 訂單狀態 :
     const orderStatusTitle = document.createElement("p");
     const orderStatusDiv = document.createElement("div");
+    const orderStatus = document.createElement("div");
+
+    //row1
+    const statusRow1 = document.createElement("div");
+    statusRow1.className = "status-row";
+    const circle1 = document.createElement("i");
+    circle1.setAttribute("class", "fa-sharp fa-solid fa-circle");
+    circle1.style.color = "rgb(255, 227, 155)";
+    const arrowDiv1 = document.createElement("div");
+    arrowDiv1.className = "arrow-div";
+    const arrow1 = document.createElement("i");
+    arrow1.setAttribute("class", "bi bi-arrow-down");
     const pending = document.createElement("p");
+    //row1 append child
+    statusRow1.appendChild(circle1);
+    arrowDiv1.appendChild(arrow1);
+    statusRow1.appendChild(pending);
+    //row2
+    const statusRow2 = document.createElement("div");
+    statusRow2.className = "status-row";
+    const circle2 = document.createElement("i");
+    circle2.setAttribute("class", "fa-sharp fa-solid fa-circle");
+    circle2.style.color = "rgb(255, 227, 155)";
+    const arrowDiv2 = document.createElement("div");
+    arrowDiv2.className = "arrow-div";
+    const arrow2 = document.createElement("i");
+    arrow2.setAttribute("class", "bi bi-arrow-down");
     const driverAccepts = document.createElement("p");
+    //row2 append child
+    statusRow2.appendChild(circle2);
+    arrowDiv2.appendChild(arrow2);
+    statusRow2.appendChild(driverAccepts);
+
+    //row3
+    const statusRow3 = document.createElement("div");
+    statusRow3.className = "status-row";
+    const circle3 = document.createElement("i");
+    circle3.setAttribute("class", "fa-sharp fa-solid fa-circle");
+    circle3.style.color = "rgb(255, 227, 155)";
+    //arrow
+    const arrowDiv3 = document.createElement("div");
+    arrowDiv3.className = "arrow-div";
+    const arrow3 = document.createElement("i");
+    arrow3.setAttribute("class", "bi bi-arrow-down");
     const driverDelivering = document.createElement("p");
+    //row3 append child
+    statusRow3.appendChild(circle3);
+    arrowDiv3.appendChild(arrow3);
+    statusRow3.appendChild(driverDelivering);
+
+    //row4
+    const statusRow4 = document.createElement("div");
+    statusRow4.className = "status-row";
+    const circle4 = document.createElement("i");
+    circle4.setAttribute("class", "fa-sharp fa-solid fa-circle");
+    circle4.style.color = "rgb(255, 227, 155)";
     const receiverReceived = document.createElement("p");
+    //row4 append child
+    statusRow4.appendChild(circle4);
+    statusRow4.appendChild(receiverReceived);
 
     //button for more details
     const detailsBtn = document.createElement("button");
@@ -64,42 +120,62 @@ async function allOrderStatus() {
 
     //each order
     const orderDiv = document.createElement("div");
+    const orderTextDiv = document.createElement("div");
+    orderTextDiv.className = "order-text";
     orderDiv.className = "each-order";
-    orderStatusDiv.className = "order-status";
+    orderStatusDiv.className = "order-status-div";
+    orderStatus.className = "order-status";
     pending.className = "pending";
     driverAccepts.className = "driver-accepts";
     driverDelivering.className = "driver-delivering";
     receiverReceived.className = "receiver-received";
+    //btn class
+    const btnDiv = document.createElement("div");
+    btnDiv.className = "btn-div";
+    detailsBtn.className = "details-btn";
+    cancelOrderBtn.className = "cancel-btn";
+
     //change status color
     if (order.orders_status === "pending") {
-      pending.style.color = "orange";
+      circle1.style.color = "rgb(255, 157, 29)";
+      pending.style.color = "rgb(255, 157, 29)";
     } else if (order.orders_status === "driver accepts") {
-      driverAccepts.style.color = "orange";
+      circle2.style.color = "rgb(255, 157, 29)";
+      driverAccepts.style.color = "rgb(255, 157, 29)";
     } else if (order.orders_status === "driver delivering") {
-      driverDelivering.style.color = "orange";
-    } else if (order.orders_status === "driver delivering") {
-      receiverReceived.style.color = "orange";
+      circle3.style.color = "rgb(255, 157, 29)";
+      driverDelivering.style.color = "rgb(255, 157, 29)";
+    } else if (order.orders_status === "receiver-received") {
+      circle4.style.color = "rgb(255, 157, 29)";
+      receiverReceived.style.color = "rgb(255, 157, 29)";
     }
-    orderStatusDiv.appendChild(pending);
-    orderStatusDiv.appendChild(driverAccepts);
-    orderStatusDiv.appendChild(driverDelivering);
-    orderStatusDiv.appendChild(receiverReceived);
-    orderDiv.appendChild(orderId);
-    orderDiv.appendChild(orderCode);
-    orderDiv.appendChild(createTime);
-    orderDiv.appendChild(pickUpDateTime);
-    orderDiv.appendChild(pickUpAddress);
-    orderDiv.appendChild(deliverAddress);
-    orderDiv.appendChild(animals);
-    orderDiv.appendChild(remarks);
-    orderDiv.appendChild(orderStatusTitle);
+
+    orderStatus.appendChild(statusRow1);
+    orderStatus.appendChild(arrowDiv1);
+    orderStatus.appendChild(statusRow2);
+    orderStatus.appendChild(arrowDiv2);
+    orderStatus.appendChild(statusRow3);
+    orderStatus.appendChild(arrowDiv3);
+    orderStatus.appendChild(statusRow4);
+    orderStatusDiv.appendChild(orderStatus);
+    orderTextDiv.appendChild(orderId);
+    orderTextDiv.appendChild(orderCode);
+    orderTextDiv.appendChild(createTime);
+    orderTextDiv.appendChild(pickUpDateTime);
+    orderTextDiv.appendChild(pickUpAddress);
+    orderTextDiv.appendChild(deliverAddress);
+    orderTextDiv.appendChild(animals);
+    orderTextDiv.appendChild(remarks);
+    orderTextDiv.appendChild(orderStatusTitle);
+    orderDiv.appendChild(orderTextDiv);
     orderDiv.appendChild(orderStatusDiv);
+    orderDiv.appendChild(btnDiv);
     //btn if there is drivers_id
     if (order.drivers_id == undefined) {
     } else {
-      orderDiv.appendChild(detailsBtn);
+      btnDiv.appendChild(detailsBtn);
     }
-    orderDiv.appendChild(cancelOrderBtn);
+    btnDiv.appendChild(cancelOrderBtn);
 
     detailsBtn.addEventListener("click", () => {
       window.location = `/ongoingdt.html?oid=${order.id}`;
