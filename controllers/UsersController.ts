6759aec8d1f87396d3b62bbc7fe193cf63fd70bf
Julia <beyourdetective@gmail.com>
2023-05-03 17:18:24 +0800

    mvc

---
+++ controllers/UsersController.ts
@@ -0,0 +1,11 @@
+import { UsersService } from "../services/UsersService";
+import { Request, Response } from "express";
+export class usersController{
+    constructor(private usersService: UsersService){}
+
+    loginControl =async (req: Request, res: Response) => {
+    try {
+        
+    }
+
+};
--- routers/authRoutes.ts
+++ routers/authRoutes.ts
@@ -1,8 +1,15 @@
-import { authController } from "../app";
 import express from "express";
 
 export const authRoutes = express.Router();
 
-authRoutes.get("/", authController.loginUser);
-authRoutes.get("/driver", authController.loginDriver);
+authRoutes.get("/", loginUser);
+authRoutes.get("/driver", loginDriver);
 
+async function loginUser(req: express.Request, res: express.Response) {
+  req.session.loginType = "user";
+  res.redirect("/connect/google");
+}
+async function loginDriver(req: express.Request, res: express.Response) {
+  req.session.loginType = "driver";
+  res.redirect("/connect/google");
+}
