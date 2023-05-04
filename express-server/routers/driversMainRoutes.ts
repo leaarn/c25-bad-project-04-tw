a9fea809f1f30f8a2620d3825c4ff492c8dc1545
dora dora <dora_1324@gmail.com>
2023-05-04 10:55:06 +0800

    rearrange folder structure

---
+++ express-server/routers/driversMainRoutes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { driverIsLoggedInApi } from "../utils/guard";
+import { driversMainController } from "../app"
+
+export const driversMainRoutes = express.Router();
+
+driversMainRoutes.get("/", driverIsLoggedInApi);
+driversMainRoutes.get("/get-driver-info", driverIsLoggedInApi, driversMainController.getDriverInfo);
+driversMainRoutes.get("/get-district", driverIsLoggedInApi, driversMainController.getDistricts);
+driversMainRoutes.get("/get-orders", driverIsLoggedInApi, driversMainController.getAllOrders);
+driversMainRoutes.get("/get-orders/:oid", driverIsLoggedInApi, driversMainController.getAcceptOrders);
+driversMainRoutes.get("/driver-earns/:oid", driverIsLoggedInApi, driversMainController.getDriverEarns);
+driversMainRoutes.get("/history/", driverIsLoggedInApi, driversMainController.getOrdersHistory);
+driversMainRoutes.get("/history/:oid", driverIsLoggedInApi, driversMainController.getSingleHistory);
+driversMainRoutes.get("/ongoing", driverIsLoggedInApi, driversMainController.getOngoingOrders);
+driversMainRoutes.put("/cfm-orders/:oid", driverIsLoggedInApi, driversMainController.confirmAcceptOrder);
+driversMainRoutes.put("/ongoing/:oid", driverIsLoggedInApi, driversMainController.getDriverDelivering);
+driversMainRoutes.post("/msg/:oid", driverIsLoggedInApi, driversMainController.message);
